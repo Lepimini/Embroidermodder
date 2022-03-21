9b2e7d322669d8d8ebae552105338b5df2d56636
robin-swift <robinswiftart@gmail.com>
2022-03-21 21:32:28 +0000

    missing file

---
+++ embroidermodder/translation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+r"""
+    Embroidermodder 2.
+
+    ------------------------------------------------------------
+
+    Copyright 2013-2022 The Embroidermodder Team
+    Embroidermodder 2 is Open Source Software.
+    See LICENCE for licensing terms.
+
+    ------------------------------------------------------------
+
+    The translation table.
+"""
+
+translation_table = {
+
+}
+
